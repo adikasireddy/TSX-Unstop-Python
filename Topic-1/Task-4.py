d03aff6b6bd64cd34a8e69483c78f15b4d8b2bc9
adikasi reddy <adikasireddy7036@gmail.com>
2025-05-17 22:51:17 +0530

    Delete Topic-1 directory

--- Topic-1/Task-4.py
+++
@@ -1,4 +0,0 @@
-print("Testing in interpreter")
-2 + 6
-#name = "Adi"
-#print("Hi " + name)
--- Topic-1/Task-5.py
+++
@@ -1,5 +0,0 @@
-name = "Adikasireddy"
-
-print("Hello! My name is", name)
-
-print("Welcome to Python programming!")
