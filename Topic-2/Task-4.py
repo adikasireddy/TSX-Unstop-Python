f154bce06e669ae67c2e05556b925b7e20a34a56
adikasi reddy <adikasireddy7036@gmail.com>
2025-05-17 22:53:59 +0530

    Add files via upload

---
+++ Topic-2/Task-4.py
@@ -0,0 +1,18 @@
+# Arithmetic operations
+>>> 5 + 3
+>>> 10 - 4
+>>> 6 * 2
+>>> 9 / 3
+>>> 10 // 3
+>>> 2 ** 3
+>>> 10 % 4
+
+# Comparison
+>>> 5 > 3
+>>> 4 == 4
+>>> 7 != 2
+
+# Logical
+>>> True and False
+>>> False or True
+>>> not True
---
+++ Topic-2/Task-5.py
@@ -0,0 +1,4 @@
+first_name = "Adi"
+last_name = "kasireddy"
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
